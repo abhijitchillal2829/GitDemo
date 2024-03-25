35a55f4afffc7af57f96fe1ca3a5d81a4d1dcb3a
Ramesh <abhijit.chillal@apmosys.com>
2024-03-25 12:35:08 +0530

    This is Modified Git Code By Me

---
+++ src/main/java/Com/Banking/PageClass/TC_003_Banking_GitCode.java
@@ -0,0 +1,10 @@
+package Com.Banking.PageClass;
+
+public class TC_003_Banking_GitCode {
+	
+	public void Sample() {
+		
+		System.out.println("This is sample page class for git");
+	}
+
+}
---
+++ src/test/java/Com/Banking/TestCases/TC_003_GitCode.java
@@ -0,0 +1,20 @@
+package Com.Banking.TestCases;
+
+import org.testng.annotations.Test;
+
+import Com.Banking.PageClass.TC_003_Banking_GitCode;
+
+public class TC_003_GitCode {
+	
+	@Test
+	 public void TC_003_Banking_GitCode() {
+		
+		TC_003_Banking_GitCode gc = new TC_003_Banking_GitCode();
+		
+		gc.Sample();
+		 
+		System.out.println("This is new test case for git");
+		 
+	 }
+
+}
