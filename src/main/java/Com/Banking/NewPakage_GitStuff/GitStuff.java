f6e59e1185dc274bbffa05c2858455282c7ab32a
Ramesh <abhijit.chillal@apmosys.com>
2024-03-25 13:51:41 +0530

    THis C:\Users\Ramesh\eclipse-workspace\GitStuff

---
+++ src/main/java/Com/Banking/NewPakage_GitStuff/GitStuff.java
@@ -0,0 +1,11 @@
+package Com.Banking.NewPakage_GitStuff;
+
+public class GitStuff {
+
+	
+	public void GitStuff_1() {
+		
+		System.out.println("This is development from gitstuff in framework");
+		
+	}
+}
