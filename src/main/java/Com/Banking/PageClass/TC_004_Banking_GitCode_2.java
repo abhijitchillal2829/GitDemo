4f42360f85bd6e4c5bbcd2d4d2f1e519749cc4b9
Ramesh <abhijit.chillal@apmosys.com>
2024-03-25 13:07:46 +0530

    This is changes made by another person

---
+++ src/main/java/Com/Banking/PageClass/TC_004_Banking_GitCode_2.java
@@ -0,0 +1,10 @@
+package Com.Banking.PageClass;
+
+public class TC_004_Banking_GitCode_2 {
+	
+	public void Sample_From_AnotherPerson() {
+		
+		System.out.println("This is sample page class for git from another person");
+	}
+
+}
---
+++ src/test/java/Com/Banking/TestCases/TC_004_GitCode_2.java
@@ -0,0 +1,17 @@
+package Com.Banking.TestCases;
+
+import Com.Banking.PageClass.TC_004_Banking_GitCode_2;
+
+public class TC_004_GitCode_2 {
+	
+	 public void TC_004_Banking_GitCode_2() {
+			
+		 TC_004_Banking_GitCode_2 gcg = new TC_004_Banking_GitCode_2();
+			
+			gcg.Sample_From_AnotherPerson();
+			 
+			System.out.println("This is new test case for git from Another Person");
+			 
+		 }
+
+}
